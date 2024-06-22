bdad6a5a1812d4a32d9622954fcb077418d7bfff
seungwon <swlee173@gmail.com>
2024-06-22 18:51:20 +0900

    init

---
+++ src/main/java/com/example/sbb/DoitSbbAppProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoitSbbAppProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoitSbbAppProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sbb/DoitSbbAppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoitSbbAppProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
