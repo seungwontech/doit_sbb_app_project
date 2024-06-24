ec393142fd4f0927bd71e9ae435c8c70b06300bf
seungwon <swlee173@gmail.com>
2024-06-24 16:20:26 +0900

    04강 서비스 클래스, 타임리프

---
+++ src/main/java/com/example/sbb/question/QuestionService.java
@@ -0,0 +1,28 @@
+package com.example.sbb.question;
+
+
+import com.example.sbb.DataNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getQuestionList() {
+        return questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
+    }
+}
