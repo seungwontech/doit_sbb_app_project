759ced605521fdf956ca4c1adf6ab10c2ec7f706
seungwon <swlee173@gmail.com>
2024-07-05 15:44:26 +0900

    08강 로그인, 로그아웃

--- src/main/java/com/example/sbb/user/UserRepository.java
+++ src/main/java/com/example/sbb/user/UserRepository.java
@@ -2,5 +2,8 @@ package com.example.sbb.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<SiteUser,Long> {
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<SiteUser, Long> {
+    Optional<SiteUser> findByusername(String username);
 }
